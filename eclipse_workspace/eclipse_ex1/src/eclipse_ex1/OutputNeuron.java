3ac04c743690212e0afa5aca3f55b7e7416c5bc7
面﨑祐一 <Uone@Omozaki-no-MacBook-Air.local>
2018-10-05 19:02:18 +0900

    BigDecimal

--- eclipse_workspace/eclipse_ex1/src/eclipse_ex1/OutputNeuron.java
+++ eclipse_workspace/eclipse_ex1/src/eclipse_ex1/OutputNeuron.java
@@ -1,59 +1,60 @@
 package eclipse_ex1;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 
 public class OutputNeuron {
 	//member field
-	double[] weight;
-	double threshoud;
-	double net = 0;
-	double eta;
-	double alpha;
-	double[] delta_W;
-	double delta_T;
+	BigDecimal[] weight;
+	BigDecimal threshoud;
+	BigDecimal net;
+	BigDecimal eta;
+	BigDecimal alpha;
+	BigDecimal[] delta_W;
+	BigDecimal delta_T;
 
 	//member method
-	public double[] getWeight(){
+	public BigDecimal[] getWeight(){
 		return weight;
 	}
 	public void clearNet(){
-		net = 0;
+		net = new BigDecimal("0.0");
 	}
-	public void setNet(double o_fromInter, int interNumber){
-		net += weight[interNumber] * o_fromInter;
+	public void setNet(BigDecimal o_fromInter, int interNumber){
+		net = net.add(weight[interNumber].multiply(o_fromInter));
 	}
 	public void calcNet(){
-		net += threshoud;
+		net = net.add(threshoud);
 	}
-	public double output(){
+	public BigDecimal output(){
 		return sigmoid(net);
 	}
-	public static double sigmoid(double net) {
-		return 1/(1 + Math.exp((-1)*net));
+	public static BigDecimal sigmoid(BigDecimal net) {
+		double s = 1 / (1 + Math.exp((-1.0) * Double.parseDouble(String.valueOf(net))));
+		return BigDecimal.valueOf(s);
 	}
-
-	public void reWeight(double y, double o_fromOutput, double o_fromInter[]){
+	public void reWeight(BigDecimal y, BigDecimal o_fromOutput, BigDecimal o_fromInter[]){
 		for(int i=0; i<weight.length; i++){
-			delta_W[i] *= alpha;
-			delta_W[i] += eta * (y - o_fromOutput) * o_fromOutput * (1 - o_fromOutput) * o_fromInter[i];
-			weight[i] += delta_W[i];	//重み更新式
+			delta_W[i] = delta_W[i].multiply(alpha);
+			delta_W[i] = delta_W[i].add(eta.multiply((y.subtract(o_fromOutput)).multiply(o_fromOutput.multiply((new BigDecimal("1.0").subtract(o_fromOutput)).multiply(o_fromInter[i])))));
+			weight[i] = weight[i].add(delta_W[i]);	//重み更新式
 		}
-		delta_T *= alpha;
-		delta_T += eta * (y - o_fromOutput) * o_fromOutput * (1 - o_fromOutput);
-		threshoud += delta_T;	//しきい値更新式
+		delta_T = delta_T.multiply(alpha);
+		delta_T = delta_T.add(eta.multiply((y.subtract(o_fromOutput)).multiply(o_fromOutput.multiply((new BigDecimal("1.0").subtract(o_fromOutput))))));
+		threshoud = threshoud.add(delta_T);	//しきい値更新式
 	}
 
 	//constructor
 	OutputNeuron(int interNumber){
-		weight = new double[interNumber];
+		weight = new BigDecimal[interNumber];
 		for(int i=0; i<interNumber; i++){
-			weight[i] = 0.5;
+			weight[i] = new BigDecimal("0.5");
 		}
-		threshoud = 0.5;
-		eta = 0.5;
-		alpha = 0.9;
-		delta_W = new double[weight.length];
-		Arrays.fill(delta_W, 0);
-		delta_T = 0;
+		threshoud = new BigDecimal("0.5");
+		eta = new BigDecimal("0.5");
+		alpha = new BigDecimal("0.9");
+		delta_W = new BigDecimal[weight.length];
+		Arrays.fill(delta_W, new BigDecimal("0.0"));
+		delta_T = new BigDecimal("0.0");
 	}
 }
